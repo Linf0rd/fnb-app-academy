bdfead9b3c213cb15ba83d723fded4fa3fb034ed
Linf0rd <linfordsnagger@gmx.com>
2025-06-21 14:07:44 +0200

    Add python1.py with examples of variable naming conventions.

---
+++ week-7/python1.py
@@ -0,0 +1,15 @@
+# Variables
+
+my_variable = 10
+total_count = 0
+user = 'John'
+
+
+# Invalid variable name
+
+# 2nd_variable = 10  # Invalid: starts with a number
+
+
+# Valid variable name
+
+second_variable = 20  # Valid: starts with a letter
