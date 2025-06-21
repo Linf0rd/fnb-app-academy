6ab201c72ad44a5b3777827ff02662ce406d671c
Linf0rd <linfordsnagger@gmx.com>
2025-06-21 15:19:01 +0200

    Add python5.py with examples of loops: for and while loops.

---
+++ week-7/python5.py
@@ -0,0 +1,34 @@
+# Lesson 7: Loops
+
+# Example 1 - For Loop
+
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)  # Output: apple, banana, cherry (each on a new line)
+
+
+
+# Example 2
+
+numbers = [1, 2, 3, 4, 7]
+
+for number in numbers:
+    print(numbers) # Output: [1, 2, 3, 4, 7] (the entire list printed each time)
+
+
+# Example 3
+
+for number in numbers:
+    print(number)  # Output: 1, 2, 3, 4, 7 (each number printed on a new line)
+
+
+# Example 4 - While Loop
+
+count = 1
+
+while count <= 5:
+    print(count)  # Output: 1, 2, 3, 4, 5 (each number printed on a new line)
+    count += 1  # Increment count by 1
+
+
