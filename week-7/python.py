37d976bc98adfe534feaa674133ee365a01b08c6
Linf0rd <linfordsnagger@gmx.com>
2025-06-21 14:02:55 +0200

    Add python.py with examples of numeric data types: integer and float.

---
+++ week-7/python.py
@@ -0,0 +1,17 @@
+# Numeric Data
+
+# Example 1 - Integer & type of Integer
+
+num = 3
+
+print(num)
+
+print(type(num))
+
+# Example 2 - Float & type of Float
+
+num2 = 3.14
+
+print(num2)
+
+print(type(num2))
