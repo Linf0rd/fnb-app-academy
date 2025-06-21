a4a2282ff3b660a83c417d0731127f78126486f8
Linf0rd <linfordsnagger@gmx.com>
2025-06-21 10:50:37 +0200

    Add strings.py with examples of single, double, and triple-quoted strings.

---
+++ week-7/strings.py
@@ -0,0 +1,18 @@
+# Strings
+
+# Example 1 - Single-quoted string
+message = 'Hello, World'
+
+print(message)
+
+# Example 2 - Double-quoted string
+
+message = "Bob's World"
+
+print(message)
+
+# Example 3 - Triple-quoted string
+message = """Bob's World
+is cool"""
+
+print(message)
