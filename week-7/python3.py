6cb88c08c97665c38da21464cad4136fdc255cbb
Linf0rd <linfordsnagger@gmx.com>
2025-06-21 14:59:41 +0200

    Add python3.py with examples of string operators: concatenation and repetition.

---
+++ week-7/python3.py
@@ -0,0 +1,18 @@
+# Lesson: 7 - Operators with Strings
+
+# Example 1
+str1 = "Hello"
+str2 = "World"
+
+print("Concatenation:", str1 + " " + str2)  # Output: Hello World
+
+
+# Example 2
+print("Repetition:", str1+ " " + str2 + " " + str2) # Output: Hello World World
+
+
+# Example 3
+
+print("Repetition with multiplication:", str1 * 3)  # Output: HelloHelloHello
+
+
