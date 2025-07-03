011cc4464483ba6042a49ef3ab4b4ac9aac80c9f
Linf0rd <linfordsnagger@gmx.com>
2025-07-03 13:54:47 +0200

    Add modules_builtin.py for lesson on modules and built-in functions.

---
+++ week-9/modules_builtin.py
@@ -0,0 +1,14 @@
+# Lesson 3 - Modules and Builtin Functions
+
+import math
+import random
+import datetime
+import os
+import sys
+
+
+print(math.pi)  # Accessing the value of pi from the math module
+print(random.randint(1, 100))  # Generating a random integer between 1 and 100
+print(datetime.datetime.now())  # Getting the current date and time
+print(os.getcwd())  # Getting the current working directory
+print(sys.version)  # Getting the Python version
